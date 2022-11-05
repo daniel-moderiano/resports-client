685a5dc99ecf9ff72d9485c34a2478783a1e8034
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-11-05 17:38:14 +1030

    Configure Jest to handle TypeScript and React Component unit tests

---
+++ jest.setup.js
@@ -0,0 +1,20 @@
+// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+
+// Used for __tests__/testing-library.js
+// Learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom/extend-expect";
+
+// Imports and setup to expose TextEncoder as global modules in jsdom test-env (it is not provided by default)
+import * as util from "util";
+
+// ref: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+// ref: https://github.com/jsdom/jsdom/issues/2524
+Object.defineProperty(window, "TextEncoder", {
+  writable: true,
+  value: util.TextEncoder,
+});
+Object.defineProperty(window, "TextDecoder", {
+  writable: true,
+  value: util.TextDecoder,
+});
