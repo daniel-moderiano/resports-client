b35e5fe436b89fb29ef95287fd6f8d20308a361f
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-11-08 16:34:06 +1030

    Switch twitch channels and videos to simpler twitch/.../id page organisation

--- pages/youtube/search.tsx
+++ pages/youtube/search.tsx
@@ -1,14 +1,10 @@
-import { TwitchSearchTab } from "features/search";
 import { YouTubeSearchTab } from "features/search";
 import { useRouter } from "next/router";
-import { useState } from "react";
 import { isValidSearchQuery, sanitiseSearchQuery } from "utils/queryHandling";
 
 const Search = () => {
   const router = useRouter();
   const UrlQuery = router.query;
-
-  const [activeTab, setActiveTab] = useState("twitch");
 
   // Avoid displaying the default search results page. This alleviates the need to conditionally run the API queries based on checking for valid queries.
   if (!isValidSearchQuery(UrlQuery)) {
