aadca541c20fd76191c3603e76c3aabe032d3d1c
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-11-18 20:01:34 +1030

    Implement all previously set-out tests for keyboard funcitonality

--- features/players/components/__tests__/VideoSettings.test.tsx
+++ features/players/components/__tests__/VideoSettings.test.tsx
@@ -94,27 +94,81 @@ describe("Options rendering and submenus", () => {
 });
 
 describe("Keyboard accessibility", () => {
+  // Menu should be of the form
+  // - Quality
+  //      - Auto
+  //      - 1080p60
+  //      - 720p60
+  // - Playback Speed
+  //      - 1
+  //      - 2
   it("Allows user to navigate primary menu options with up/down arrow keys", async () => {
     setup();
+    const playbackSpeedButton = screen.getByRole("menuitem", {
+      name: /playback/i,
+    });
+    await userEvent.keyboard("ArrowDown");
+    expect(playbackSpeedButton).toHaveFocus();
   });
 
   it("Allows user to navigate primary menu options with tab key", async () => {
     setup();
+    const playbackSpeedButton = screen.getByRole("menuitem", {
+      name: /playback/i,
+    });
+    await userEvent.keyboard("Tab");
+    expect(playbackSpeedButton).toHaveFocus();
   });
 
   it("Allows users to open submenus with RightArrow press", async () => {
     setup();
+    await userEvent.keyboard("ArrowRight");
+    // Looking for a specific submenu item
+    const qualityOption = screen.getByRole("menuitem", {
+      name: /1080p/i,
+    });
+    expect(qualityOption).toBeInTheDocument();
   });
 
   it("Allows user to navigate submenu options with up/down arrow keys", async () => {
     setup();
+    const qualityButton = screen.getByRole("menuitem", {
+      name: /quality/i,
+    });
+    await userEvent.click(qualityButton);
+    await userEvent.keyboard("ArrowDown");
+    // Second in submenu list
+    const hd1080p = screen.getByRole("menuitem", {
+      name: /1080p/i,
+    });
+    expect(hd1080p).toHaveFocus();
   });
 
   it("Allows user to navigate submenu options with tab key", async () => {
     setup();
+    const qualityButton = screen.getByRole("menuitem", {
+      name: /quality/i,
+    });
+    await userEvent.click(qualityButton);
+    await userEvent.keyboard("Tab");
+    // Second in submenu list
+    const hd1080p = screen.getByRole("menuitem", {
+      name: /1080p/i,
+    });
+    expect(hd1080p).toHaveFocus();
   });
 
   it("Allows users to close submenus with LeftArrow press", async () => {
     setup();
+    const qualityButton = screen.getByRole("menuitem", {
+      name: /quality/i,
+    });
+    await userEvent.click(qualityButton);
+    await userEvent.keyboard("ArrowLeft");
+
+    const hd1080p = screen.queryByRole("menuitem", {
+      name: /1080p/i,
+    });
+    expect(hd1080p).not.toBeInTheDocument();
   });
 });
