4f35e12f6bd408bea3c75cc4dd6ae93710004792
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-11-25 21:15:10 +1030

    Adjust volume level to fixed 0 decimal places

--- features/players/components/VolumeLevelIndicator.tsx
+++ features/players/components/VolumeLevelIndicator.tsx
@@ -7,7 +7,7 @@ interface VolumeLevelIndicatorProps {
 export const VolumeLevelIndicator = ({
   currentVolume,
 }: VolumeLevelIndicatorProps) => {
-  const volumeAsPercentage = `${currentVolume * 100}%`;
+  const volumeAsPercentage = `${(currentVolume * 100).toFixed(0)}%`;
 
   return (
     <div
