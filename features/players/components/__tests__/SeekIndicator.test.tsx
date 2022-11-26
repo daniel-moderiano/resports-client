60fdecbe32dd84ee07cfd454b48eae948b5187a6
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-11-27 09:20:27 +1030

    Define all tests expected for the seek indicator

--- features/players/components/__tests__/SeekIndicator.test.tsx
+++ features/players/components/__tests__/SeekIndicator.test.tsx
@@ -1,16 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import { SeekIndicator } from "../SeekIndicator";
 
-describe("Volume level indicator UI", () => {
-  it("Shows correct volume percentage", () => {
-    render(<SeekIndicator projectedSeekInSeconds={5} />);
-    const volumeLevel = screen.getByText("50%");
-    expect(volumeLevel).toBeInTheDocument();
+describe("Seek indicator UI", () => {
+  it.todo("Shows correct number of seconds to seek (for seek < 1 min)", () => {
+    render(<SeekIndicator projectedSeekInSeconds={10} />);
   });
 
-  it("Has correct accessible description", () => {
-    render(<SeekIndicator projectedSeekInSeconds={30} />);
-    const volumeLabel = screen.getByLabelText("Volume level 50%");
-    expect(volumeLabel).toBeInTheDocument();
+  it.todo("Shows correct number of minutes to seek (for seek > 1 min)", () => {
+    render(<SeekIndicator projectedSeekInSeconds={300} />);
   });
+
+  it.todo("Has correct accessible description for seeking forward", () => {
+    render(<SeekIndicator projectedSeekInSeconds={30} />);
+  });
+
+  it.todo("Has correct accessible description for seeking backward", () => {
+    render(<SeekIndicator projectedSeekInSeconds={-30} />);
+  });
+
+  it.todo(
+    "Renders right side indicator and facing icon for forward seek",
+    () => {
+      render(<SeekIndicator projectedSeekInSeconds={30} />);
+    }
+  );
+
+  it.todo(
+    "Renders left side indicator and facing icon for backward seek",
+    () => {
+      render(<SeekIndicator projectedSeekInSeconds={-30} />);
+    }
+  );
 });
