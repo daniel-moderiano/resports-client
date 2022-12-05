4cdfd28da3cf5242fcf278255b8f02b1c7715fda
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-12-05 11:43:42 +1030

    Code some initial ideas around using a native input over a custom progress bar

--- features/players/components/video-controls/VolumeSlider.tsx
+++ features/players/components/video-controls/VolumeSlider.tsx
@@ -16,18 +16,46 @@ export const VolumeSlider = ({ player }: VolumeSliderProps) => {
   }, [currentPlayerVolume]);
 
   return (
-    <input
-      type="range"
-      min={0}
-      max={100}
-      step={1}
-      value={volume}
-      onChange={(event) => {
-        setVolume(event.target.valueAsNumber);
-        player.setVolume(event.target.valueAsNumber);
-      }}
-      data-testid="slider"
-      className={styles.slider}
-    />
+    <>
+      <div className={styles.inputContainer}>
+        <div className={styles.progress}></div>
+        <input
+          type="range"
+          min={0}
+          max={100}
+          step={1}
+          value={volume}
+          onChange={(event) => {
+            setVolume(event.target.valueAsNumber);
+            player.setVolume(event.target.valueAsNumber);
+          }}
+          data-testid="slider"
+          className={styles.slider}
+        />
+      </div>
+
+      {/* <div
+        role="slider"
+        className={styles.customSlider}
+        tabIndex={0}
+        aria-valuenow={0.292}
+        aria-valuetext="29% volume"
+        aria-label="Volume (use up/down arrow keys to change)"
+        aria-valuemin={0}
+        aria-valuemax={1}
+        // style="display: none;"
+      >
+        <div className={styles.volumeBar}>
+          <div
+            className={styles.volumeBarFill}
+            // style="height: 29.2%;"
+          ></div>
+          <div
+            className={styles.sliderHandle}
+            // style="bottom: 29.2%;"
+          ></div>
+        </div>
+      </div> */}
+    </>
   );
 };
