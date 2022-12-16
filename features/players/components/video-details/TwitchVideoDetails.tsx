29236853eb1435f796dc98fa4e02b8c4b63ee04d
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-12-16 17:42:02 +1030

    Remove absolute twitch link positioning to improve responsiveness

--- features/players/components/video-details/TwitchVideoDetails.tsx
+++ features/players/components/video-details/TwitchVideoDetails.tsx
@@ -16,27 +16,29 @@ export const TwitchVideoDetails = ({ videoId }: TwitchVideoDetailsProps) => {
     <div className={styles.outerContainer}>
       {data && (
         <section className={styles.innerContainer}>
-          <h2 className={styles.videoTitle}>{data.videoData.title}</h2>
+          <div className={styles.videoDetails}>
+            <h2 className={styles.videoTitle}>{data.videoData.title}</h2>
 
-          <div className={styles.metricsContainer}>
-            <p className={styles.uploadedDate}>
-              Uploaded {data.videoData.creationDate.toLocaleDateString()}
-            </p>
-            <span>·</span>
-            <p className={styles.views}>{data.videoData.views} views</p>
-          </div>
-          <div className={styles.channelContainer}>
-            <Image
-              alt={`${data.userData.displayName} thumbnail`}
-              src={data.userData.profilePictureUrl}
-              width={55}
-              height={55}
-              className={styles.channelThumbnail}
-            />
-            <span className={styles.channelName}>
-              {data.userData.displayName}
-            </span>
-            <button className={styles.subscribeButton}>Subscribe</button>
+            <div className={styles.metricsContainer}>
+              <p className={styles.uploadedDate}>
+                Uploaded {data.videoData.creationDate.toLocaleDateString()}
+              </p>
+              <span>·</span>
+              <p className={styles.views}>{data.videoData.views} views</p>
+            </div>
+            <div className={styles.channelContainer}>
+              <Image
+                alt={`${data.userData.displayName} thumbnail`}
+                src={data.userData.profilePictureUrl}
+                width={55}
+                height={55}
+                className={styles.channelThumbnail}
+              />
+              <span className={styles.channelName}>
+                {data.userData.displayName}
+              </span>
+              <button className={styles.subscribeButton}>Subscribe</button>
+            </div>
           </div>
           <Link className={styles.twitchLink} href={data.videoData.url}>
             <NewTabIcon className={styles.newTabIcon} />
