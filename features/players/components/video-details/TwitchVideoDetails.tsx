2256fe1cfc23659b9d0bbde746e04a4a70dbbcc9
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-12-16 17:36:34 +1030

    Add link to watch video on twitch

--- features/players/components/video-details/TwitchVideoDetails.tsx
+++ features/players/components/video-details/TwitchVideoDetails.tsx
@@ -2,6 +2,8 @@ import { useGetTwitchVideoDetails } from "features/players/hooks/useGetTwitchVid
 import React, { useEffect } from "react";
 import Image from "next/image";
 import styles from "features/players/components/styles/TwitchVideoDetails.module.css";
+import Link from "next/link";
+import NewTabIcon from "icons/NewTabIcon";
 
 interface TwitchVideoDetailsProps {
   videoId: string;
@@ -11,23 +13,24 @@ export const TwitchVideoDetails = ({ videoId }: TwitchVideoDetailsProps) => {
   const { isError, isLoading, data } = useGetTwitchVideoDetails(videoId);
 
   return (
-    <>
+    <div className={styles.outerContainer}>
       {data && (
-        <section className={styles.container}>
+        <section className={styles.innerContainer}>
           <h2 className={styles.videoTitle}>{data.videoData.title}</h2>
+
           <div className={styles.metricsContainer}>
             <p className={styles.uploadedDate}>
               Uploaded {data.videoData.creationDate.toLocaleDateString()}
             </p>
-            <span>|</span>
+            <span>·</span>
             <p className={styles.views}>{data.videoData.views} views</p>
           </div>
           <div className={styles.channelContainer}>
             <Image
               alt={`${data.userData.displayName} thumbnail`}
               src={data.userData.profilePictureUrl}
-              width={65}
-              height={65}
+              width={55}
+              height={55}
               className={styles.channelThumbnail}
             />
             <span className={styles.channelName}>
@@ -35,8 +38,12 @@ export const TwitchVideoDetails = ({ videoId }: TwitchVideoDetailsProps) => {
             </span>
             <button className={styles.subscribeButton}>Subscribe</button>
           </div>
+          <Link className={styles.twitchLink} href={data.videoData.url}>
+            <NewTabIcon className={styles.newTabIcon} />
+            Watch on Twitch
+          </Link>
         </section>
       )}
-    </>
+    </div>
   );
 };
