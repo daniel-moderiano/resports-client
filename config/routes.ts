bf5bc6d4061a20ec8093c8d1f9fd805375ab7483
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-12-27 19:45:52 +1030

    Create centralised routes config file to be imported across app

---
+++ config/routes.ts
@@ -0,0 +1,18 @@
+export const Routes = {
+  home: "/",
+  search: {
+    twitch: "/twitch/search",
+    youtube: "/youtube/search",
+  },
+  channel: {
+    twitch: "/twitch/channel",
+    youtube: "/youtube/channel",
+  },
+  video: {
+    twitch: "/twitch/video",
+    youtube: "/youtube/video",
+  },
+  about: "/about",
+  settings: "/settings",
+  savedChannels: "/saved",
+};
