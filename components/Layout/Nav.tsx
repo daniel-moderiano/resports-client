4ab5802a58e5d14b2773a2aed96b9712ddd89b45
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-12-28 15:07:58 +1030

    Abstract UserMenu to contain authenticated user options

--- components/Layout/Nav.tsx
+++ components/Layout/Nav.tsx
@@ -3,7 +3,6 @@ import { Routes } from "config/routes";
 import { SearchBar } from "features/search";
 import HamburgerIcon from "icons/HamburgerIcon";
 import Link from "next/link";
-import { useState } from "react";
 import { UserMenu } from "./UserMenu";
 
 // In the future we will access the user property from React context or similar. This is a placeholder to allow development of the nav menu in authenticated and non-authenticated states while we await backend integration.
@@ -16,7 +15,6 @@ interface NavProps {
 }
 
 export const Nav = ({ showSidebar, toggleSidebar }: NavProps) => {
-  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className={styles.header}>
       <div className={styles.leftContainer}>
@@ -34,20 +32,16 @@ export const Nav = ({ showSidebar, toggleSidebar }: NavProps) => {
             Resports
           </Link>
         </h1>
-      </div>{" "}
+      </div>
       <SearchBar />
       <div className={styles.rightContainer}>
         {user ? (
-          <>{showMenu && <UserMenu />}</>
+          <UserMenu />
         ) : (
-          <ul>
-            <li>
-              <Link href={Routes.login}>Log In</Link>
-            </li>
-            <li>
-              <Link href={Routes.signup}>Sign Up</Link>
-            </li>
-          </ul>
+          <div>
+            <Link href={Routes.login}>Log In</Link>
+            <Link href={Routes.signup}>Sign Up</Link>
+          </div>
         )}
       </div>
     </nav>
--- components/Layout/UserMenu.tsx
+++ components/Layout/UserMenu.tsx
@@ -1,19 +1,42 @@
 import { Routes } from "config/routes";
+import CaretIcon from "icons/CaretIcon";
 import Link from "next/link";
+import { useState } from "react";
 
 export const UserMenu = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <ul role="menu" aria-label="User navigation menu">
-      <li role="none">
-        <Link role="menuitem" href={Routes.login}>
-          Log In
-        </Link>
-      </li>
-      <li role="none">
-        <Link role="menuitem" href={Routes.signup}>
-          Sign Up
-        </Link>
-      </li>
-    </ul>
+    <div>
+      <button
+        type="button"
+        aria-controls="dropdown"
+        aria-haspopup="true"
+        aria-expanded={showMenu}
+        onClick={() => {
+          setShowMenu(!showMenu);
+        }}
+      >
+        <div>
+          <span id="dropdown-me" className="text-xs">
+            Me
+          </span>
+          <CaretIcon />
+        </div>
+      </button>
+      {showMenu && (
+        <ul role="menu" aria-label="User navigation menu">
+          <li role="none">
+            <Link role="menuitem" href={Routes.login}>
+              Log In
+            </Link>
+          </li>
+          <li role="none">
+            <Link role="menuitem" href={Routes.signup}>
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      )}
+    </div>
   );
 };
