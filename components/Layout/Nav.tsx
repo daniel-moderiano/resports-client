24625746be07d9072951bf0517e5cfc99789590b
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-12-28 11:30:10 +1030

    Create nav conditional for dynamic nav menu depending on user auth state

--- components/Layout/Nav.tsx
+++ components/Layout/Nav.tsx
@@ -1,12 +1,24 @@
-// * If anchor tags are to be placed in the list items, these should become role="menuitem" and the <li> tags should have role="none"
-
-// The Nav is also likely to contain a search bar and a logo, that links back to the home page
 import styles from "components/Layout/styles/Nav.module.css";
+import { Routes } from "config/routes";
+import Link from "next/link";
+
+// In the future we will access the user property from React context or similar. This is a placeholder to allow development of the nav menu in authenticated and non-authenticated states while we await backend integration.
+
+const user = false;
 
 export const Nav = () => {
-  return (
-    <nav role="navigation" className={styles.nav}>
-      Nav
-    </nav>
-  );
+  if (user) {
+    return (
+      <nav role="navigation" className={styles.nav}>
+        {/* Dropdown menu */}
+      </nav>
+    );
+  } else {
+    return (
+      <nav role="navigation" className={styles.nav}>
+        <Link href={"?"}>Log In</Link>
+        <Link href={"?"}>Sign Up</Link>
+      </nav>
+    );
+  }
 };
