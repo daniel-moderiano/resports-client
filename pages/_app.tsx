51fa10d9bee8a61e951ebc0a0cf474e7efd8ac57
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-17 11:41:48 +1030

    Add Auth0 Provider to wrap application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,27 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Layout } from "components/Layout";
 import { GapiContextProvider } from "providers/GapiContext";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <GapiContextProvider>
-      <QueryClientProvider client={queryClient}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-      </QueryClientProvider>
-    </GapiContextProvider>
+    <Auth0Provider
+      domain="dev-c0yb5cr7.us.auth0.com"
+      clientId="mJvb9a6rBcUFG9UnkLDwt8ODyCACYFsI"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <GapiContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+        </QueryClientProvider>
+      </GapiContextProvider>
+    </Auth0Provider>
   );
 }
