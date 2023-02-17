c22b0e87fea5b219de495f0898d788b8c60481c1
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-17 19:39:48 +1030

    Add aria-current logic to new login button

--- components/Auth0/LoginButton.tsx
+++ components/Auth0/LoginButton.tsx
@@ -1,10 +1,14 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+import * as React from "react";
 
-const LoginButton = () => {
+const LoginButton = (props: React.HTMLAttributes<HTMLButtonElement>) => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <button {...props} onClick={() => loginWithRedirect()}>
+      Log In
+    </button>
+  );
 };
 
 export default LoginButton;
