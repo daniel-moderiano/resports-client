c0fd410c28dc82e1b6e3907372fe6041ba73468a
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-03-16 18:21:45 +1030

    Remove auth0 profile info on home page

--- features/auth/components/Profile.tsx
+++ features/auth/components/Profile.tsx
@@ -2,8 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import * as React from "react";
 
 export const Profile = () => {
-  const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
-    useAuth0();
+  const { user, getAccessTokenSilently } = useAuth0();
   const [userMetadata, setUserMetadata] = React.useState<null | string>(null);
 
   React.useEffect(() => {
@@ -19,21 +18,8 @@ export const Profile = () => {
     getUserMetadata();
 
     console.log(JSON.stringify(user));
-  }, [getAccessTokenSilently, user]);
+    console.log(userMetadata);
+  }, [getAccessTokenSilently, user, userMetadata]);
 
-  if (isLoading) {
-    return <div>Loading ...</div>;
-  }
-
-  return isAuthenticated && user ? (
-    <div>
-      <img src={user.picture} alt={user.name} />
-      <h2>{user.name}</h2>
-      <p>{user.email}</p>
-      <h3>User Metadata</h3>
-      {userMetadata ? <p>{userMetadata}</p> : "No user metadata defined"}
-    </div>
-  ) : (
-    <h2>You are not logged in</h2>
-  );
+  return <div></div>;
 };
