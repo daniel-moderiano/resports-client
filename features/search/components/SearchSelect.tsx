a41e372cf247d8dd4b93cb49b09ecc2e4f235915
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-03-22 10:07:14 +1030

    Fix id react-select erro by adding instanceId prop

--- features/search/components/SearchSelect.tsx
+++ features/search/components/SearchSelect.tsx
@@ -8,6 +8,7 @@ export function CustomSelect<
   return (
     <Select
       {...props}
+      instanceId="react-select"
       styles={{
         control: (provided, state) => ({
           ...provided,
