2c6ba695eecd85ba93be9cfa615f1aa092a8fbe3
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-07-15 09:06:28 +0930

    Improve types using discriminated unions

--- types/backendAPITypes.ts
+++ types/backendAPITypes.ts
@@ -33,25 +33,55 @@ export type Channel = Infer<typeof ChannelStruct>;
 export type User = Infer<typeof UserStruct>;
 export type PopulatedUser = Infer<typeof PopulatedUserStruct>;
 
-export type ApiResponseStatus = "success" | "fail" | "error";
-
-export type ApiResponse<T> = {
-  status: ApiResponseStatus;
+/**
+ * Represents successful API responses.
+ * Contains a status of 'success' and any data returned from the API.
+ */
+export type SuccessApiResponse<T> = {
+  status: "success";
   data: T;
 };
 
-export type ErrorApiResponse = ApiResponse<string | null>;
+/**
+ * Represents failed API responses.
+ * Contains a status of 'fail' and a message detailing the reason for the failure.
+ */
+export type FailApiResponse = {
+  status: "fail";
+  message: string;
+};
 
-export type FailApiResponse = ApiResponse<Record<string, string>>;
+/**
+ * A generic API response which could be either a successful or failed response.
+ *
+ * Use of discrimination unions here means that checking for status "fail" or "success"
+ * will automatically result in TypeScript identifying whether the second property is
+ * `data` or `message`.
+ */
+export type ApiResponse<T> = SuccessApiResponse<T> | FailApiResponse;
 
-export type SuccessfulDeleteApiResponse = ApiResponse<null>;
-
+/**
+ * API response type for adding a new channel.
+ * On success, it includes the newly added channel information.
+ */
 export type AddChannelApiResponse = ApiResponse<{ channel: Channel }>;
 
+/**
+ * API response type for retrieving saved channels.
+ * On success, it includes an array of the saved channels.
+ */
 export type GetSavedChannelsApiResponse = ApiResponse<{
   savedChannels: Channel[];
 }>;
 
+/**
+ * API response type for adding a new user.
+ * On success, it includes the newly added user's information.
+ */
 export type AddUserApiResponse = ApiResponse<{ user: User }>;
 
+/**
+ * API response type for adding a saved channel to a user's list.
+ * On success, it includes the updated user's information.
+ */
 export type AddSavedChannelApiResponse = ApiResponse<{ user: PopulatedUser }>;
